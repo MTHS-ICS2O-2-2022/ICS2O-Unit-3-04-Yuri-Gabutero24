a7c26c12f5c8539a0f63ac1938a37675cf7b47d9
Yuri-Gabutero24 <124618894+Yuri-Gabutero24@users.noreply.github.com>
2023-04-12 16:59:57 +0000

    webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Yuri G
+// Created on: April 2023
 // This file contains the JS functions for index.html
+
+
+'use strict'
+/**
+* This function is for converting the temperature from Fahrenheit to Celsius
+*/
+function calculate() {
+// input
+  const fahrenheit = parseInt(document.getElementById('enter-fahrenheit').value)
+
+ // process
+ const temperature = (fahrenheit - 32) * 5 / 9
+
+  // output
+  document.getElementById('celsius').innerHTML = "The temperature in Celsius is " + temperature + "°C"
+}
